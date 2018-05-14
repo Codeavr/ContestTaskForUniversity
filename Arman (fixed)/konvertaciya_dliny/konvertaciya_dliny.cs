8d3d63855eb7649db7f0f7a76f52cd9e1ece35ef
uncanny25 <arman_barakazyan@outlook.com>
2018-05-15 00:57:40 +0300

    Added 17th task

---
+++ Arman (fixed)/konvertaciya_dliny/konvertaciya_dliny.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int a = int.Parse(data[0]);
+            int k = int.Parse(data[1]);
+            switch (k)
+            {
+                case 1:
+                    Console.WriteLine((a * 1000).ToString());
+                    break;
+                case 2:
+                    Console.WriteLine((a * 10000).ToString());
+                    break;
+                case 3:
+                    Console.WriteLine((a * 100000).ToString());
+                    break;
+            }
+            Console.Read();
+        }
+    }
+}
