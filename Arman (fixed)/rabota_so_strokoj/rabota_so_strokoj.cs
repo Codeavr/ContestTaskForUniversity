65da661a294fd53dfa01b7a950dc1a6d6054e3d5
uncanny25 <arman_barakazyan@outlook.com>
2018-05-14 23:57:02 +0300

    Added 16th task

---
+++ Arman (fixed)/rabota_so_strokoj/rabota_so_strokoj.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int length = data.Length;
+            int all_length=0;
+            for (int i = 0; i < length; i++)
+            {
+                all_length += data[i].Length+1;
+            }
+            char[] b = new char[all_length];
+            int ii = 0;
+            for (int i = 0; i < length; i++)
+            {
+                foreach (char d in data[i])
+                {
+                    b[ii] = d;
+                    ii++;
+                }
+                b[ii] = ' ';
+                ii++;
+            }
+            ii--;
+            Array.Resize(ref b, ii);
+            string result = "";
+            if (length < 99999)
+            {
+                for (int i = 0; i < ii; i++)
+                {
+                    int count = 0;
+                    for (int j = 0; j < ii; j++)
+                    {
+                        if (b[i] == b[j] && (b[i]!=' ' && b[i]!='.'))
+                        {
+                            if (count > 0)
+                            {
+                                b[j] = '*';
+                            }    
+                            count++;
+                        }
+                    }
+                }   
+                for (int i = 0; i < ii; i++)
+                {
+                    if (b[i] != '*')
+                    {
+                        result += b[i].ToString();
+                    }
+                }
+                Console.WriteLine(result);
+            }
+            Console.Read();
+        }
+    }
+}
