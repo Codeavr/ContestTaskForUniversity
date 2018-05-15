5dea0033dbf98c33c34cdd8ec2cb1b587797aef9
uncanny25 <arman_barakazyan@outlook.com>
2018-05-15 21:32:31 +0300

    Added 18th task

---
+++ Arman (fixed)/sravnenie_strok/sravnenie_strok.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            string a = data[0];
+            string b = data[1];
+            if (a != b)
+            {
+                Console.WriteLine(a + b);
+            }
+            else
+            {
+                Console.WriteLine("SAME");
+            }
+            Console.Read();
+        }
+    }
+}
