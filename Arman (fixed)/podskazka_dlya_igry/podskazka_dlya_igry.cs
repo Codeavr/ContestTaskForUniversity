d63872ea15fe7c9bec5d014d775d269018385a11
uncanny25 <arman_barakazyan@outlook.com>
2018-05-15 22:16:38 +0300

    Added 20th task

---
+++ Arman (fixed)/podskazka_dlya_igry/podskazka_dlya_igry.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int x = int.Parse(data[0]);
+            string result = "";
+            if (x>=1 && x <= 9)
+            {
+                if (x >= 5)
+                {
+                    result += "x>=5 ";
+                    if (x >= 7)
+                    {
+                        result += "x>=7";
+                    }
+                    else
+                    {
+                        result += "x<=7";
+                    }
+                }
+                else
+                {
+                    result += "x<=5 ";
+                    if (x >= 3)
+                    {
+                        result += "x>=3";
+                    }
+                    else
+                    {
+                        result += "x<=3";
+                    }
+                }
+                Console.WriteLine(result);
+            }     
+            Console.Read();
+        }
+    }
+}
