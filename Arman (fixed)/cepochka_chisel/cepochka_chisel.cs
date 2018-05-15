d0c7a6fd3f01561ac48356432e79d4506c98b1c1
uncanny25 <arman_barakazyan@outlook.com>
2018-05-15 22:47:11 +0300

    Added 21th task

---
+++ Arman (fixed)/cepochka_chisel/cepochka_chisel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int n = int.Parse(data[0]);
+            int m = int.Parse(data[1]);
+            string result = "";
+            if (n<m && n>=1 && m>=1 && n<=100 && m<=100)
+            {
+                for (int i = n; i <= m; i++)
+                {
+                    result += i.ToString() + " ";
+                }
+                result = result.Substring(0, result.Length - 1);
+                Console.WriteLine(result);
+            }     
+            Console.Read();
+        }
+    }
+}
