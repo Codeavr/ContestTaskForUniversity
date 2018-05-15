fa3e9cfe8e970ae23c7ef309d83d0367a94de11b
uncanny25 <arman_barakazyan@outlook.com>
2018-05-15 21:53:49 +0300

    Added 19th task

---
+++ Arman (fixed)/poisk_cifry_v_chisle/poisk_cifry_v_chisle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int length = data[0].Length;
+            int k = int.Parse(data[1]);
+            int[] b = new int[length];
+            int _i = 0;
+            foreach (char c in data[0])
+            {
+                b[_i] = int.Parse(c.ToString());
+                _i++;
+            }
+            string result = "";
+            if (k !=0 && k<= length && length < 11)
+            {
+                result += b[k-1].ToString();
+                Console.WriteLine(result);
+            }         
+            Console.Read();
+        }
+    }
+}
