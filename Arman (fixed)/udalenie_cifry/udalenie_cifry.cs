d8a1ce0c2cccd98300c120ea4873d960f681cc38
uncanny25 <arman_barakazyan@outlook.com>
2018-05-16 21:47:31 +0300

    Added 22th task

---
+++ Arman (fixed)/udalenie_cifry/udalenie_cifry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int n = int.Parse(data[0]);
+            int m = int.Parse(data[1]);
+
+            int length = data[0].Length;
+            int[] b = new int[length];
+            int _i = 0;
+            foreach (char c in data[0])
+            {
+                b[_i] = int.Parse(c.ToString());
+                _i++;
+            }
+            string result = "";
+            if (length<11 && m<10 && m>=0)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    if (b[i] != m)
+                    {
+                        result += b[i].ToString();
+                    }
+                    
+                }
+                Console.WriteLine(result);
+            }     
+            Console.Read();
+        }
+    }
+}
