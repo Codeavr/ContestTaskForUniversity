22f2cb24fcb5b9e3f52f1dea85d4fe5258215912
uncanny25 <arman_barakazyan@outlook.com>
2018-05-16 23:08:51 +0300

    Added 24th task

---
+++ Arman (fixed)/arifmeticheskaya_progressiya/arifmeticheskaya_progressiya.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int a = int.Parse(data[0]);
+            int b = int.Parse(data[1]);
+            string result = a.ToString();
+            int m = b - a;
+            if ( m>1 && a>0 && b>0 && a<100 && b<100)
+            {
+                for (int i = a; i+m < 100;)
+                {
+                    i = i + m;
+                    result += " " + i.ToString();
+                }
+                Console.WriteLine(result);
+            }     
+            Console.Read();
+        }
+    }
+}
