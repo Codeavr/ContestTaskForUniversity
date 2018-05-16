d00dff871d6318d95e2851055c3cd060d4bd6bb6
uncanny25 <arman_barakazyan@outlook.com>
2018-05-16 23:58:00 +0300

    Added 26th task

---
+++ Arman (fixed)/poisk_chisel/poisk_chisel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int length = data.Length;
+            string result = "";
+            bool solo = true;
+            for (int i = 0; i < length; i++)
+            {
+                foreach (char c in data[i])
+                {
+                    if (Char.IsDigit(c)){
+                        result += c;
+                        solo = false;
+                    }
+                }
+                if (solo == false && i!= (length-1))
+                {
+                    result += " ";
+                    solo = true;
+                }
+            }
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
