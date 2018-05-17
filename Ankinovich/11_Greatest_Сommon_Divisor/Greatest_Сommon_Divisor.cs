0b36f31d0ac0aaecf8d3b6300d95d66e64db45e3
Sergey Ankinovich <codeavr@ya.ru>
2018-05-17 12:46:26 +0300

    Added 11th task

---
+++ Ankinovich/11_Greatest_Сommon_Divisor/Greatest_Сommon_Divisor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    public static int GetGcd(int a, int b)
+    {
+        return b != 0 ? GetGcd(b, a % b) : a;
+    }
+
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine().Split();
+
+        int gcd = GetGcd(int.Parse(input[0]), int.Parse(input[1]));
+
+        Console.WriteLine(gcd);
+    }
+}
