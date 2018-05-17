216fae4ef18620bc398441df1b4111c14b715bdf
Sergey Ankinovich <codeavr@ya.ru>
2018-05-17 08:42:15 +0300

    Moved files to home directory, added 2 tasks

---
+++ Ankinovich/01_AreaOfCircle/AreaOfCircle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double r = double.Parse(Console.ReadLine()) / 2.0;
+        Console.WriteLine(Math.Round(r * r * Math.PI, 2));
+    }
+}
