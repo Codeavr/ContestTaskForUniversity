2f00c103cee3f2827d55fc5724b31353a146dd5f
Sergey Ankinovich <codeavr@ya.ru>
2018-05-17 14:35:43 +0300

    Added 13th task

---
+++ Ankinovich/13_SubstringCount/SubstringCount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var substring = Console.ReadLine();
+        var text = Console.ReadLine();
+
+        Console.WriteLine(Regex.Matches(text, substring).Count);
+    }
+}
