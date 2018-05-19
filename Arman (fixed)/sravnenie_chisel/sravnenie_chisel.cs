b8a255e1516fc35925399c72e78aa154ba1e48da
uncanny25 <arman_barakazyan@outlook.com>
2018-05-19 17:39:44 +0300

    Added 1st part (1-15 tasks)

---
+++ Arman (fixed)/sravnenie_chisel/sravnenie_chisel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int a = int.Parse(data[0]);
+            int b = int.Parse(data[1]);
+            if (a>=0 && b>= 0 && a<=100 && b <= 100)
+            {
+                if (a > b) Console.WriteLine(a.ToString() + ">" + b.ToString());
+                if (a < b) Console.WriteLine(a.ToString() + "<" + b.ToString());
+                if (a == b) Console.WriteLine(a.ToString() + "=" + b.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Arman (fixed)/udvoenie_peremennoj_n_raz/udvoenie_peremennoj_n_raz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int a = int.Parse(data[0]);
+            int n = int.Parse(data[1]);
+            if (data[0].Length > 0 && data[0].Length < 10 && n >= 0 && n <= 10)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    a = a * 2;
+                }
+                Console.WriteLine(a.ToString());
+            } 
+            Console.Read();
+        }
+    }
+}
